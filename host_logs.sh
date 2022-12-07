138e6f09c50dee337bc30c2687d4a685caeeadc6
baimiyishu13 <baimiyishu13@163.com>
2022-12-07 15:09:51 +0800

    这将会收集指定的日志上打包传到文件服务器，提供输出下载 url

--- host_logs.sh
+++ host_logs.sh
@@ -10,7 +10,7 @@ cat << EOF
 
     # Modifications:
 
-    # Description		: 这将会收集指定的日志上传到文件服务器，提供输出下载 url
+    # Description		: 这将会收集指定的日志上打包传到文件服务器，提供输出下载 url
 EOF
 echo -e "\e[1;33m----------------------------------------------------------------------------------------------------------\e[0m"
 }
@@ -22,47 +22,53 @@ echo -e "\e[1;33m---------------------------------------------------------------
 funct_get_logs()
 {
     appids=`docker ps | grep 44671 | grep 'x86_64'  | awk '{print $NF}' | awk -F"_" '{print $NF}'`  # 解码appid
-    appid_first=`echo ${appids} | awk {'print $1'}`
-    appid_second=`echo ${appids} | awk {'print $2'}`
 
-    echo ${appids} | awk {'print $2'}
-    num=0
+    num=1
     for id in ${appids}
     do
-        echo -e "[$num]\e[1;31m${id}\e[0m"
+        if [ $num == 1 ];then
+            appid_first=$id
+            echo -e "[$num]\e[1;31m${id}\e[0m"
+        elif [ $num == 2 ];then
+            appid_second=$id
+            echo -e "[$num]\e[1;31m${id}\e[0m"
+        fi
         let num++
     done
 
     # 选择 appid
-    read -p "appid(0/1): " n
+    read -p "appid(1/2): " n
     case "${n}" in
-    0)
+    1)
         appid=${appid_first}
         ;;
-    1)
+    2)
         appid=${appid_second}
         ;;
     *)
-        echo "erro"
+        echo "error:输入1/2 选择appid"
         exit 1
     esac
     
-    echo -e "\e[1;32m执行...\e[0m"
+    echo -e "\e[1;32m脚本执行中......\e[0m"
 
     cd /mnt/lvmdisk/.onething_data/task_${appid}/storage/cde/pcdn
+
     # 取文件
-    for file in {cde-1010-backup.log,cde-1010.log,"./cde-cache-vod/c"}
+    for file in {cde-1010-backup.log,cde-1010.log,"./cde-cache-vod/test.log"}
     do
-        if [ ! -e $file ];then
-        continue
-        else
-            url=`curl -s -F upload=@$file  ${FILESERVERS}| awk -F"\"" '{print $6}'` &>/dev/null
+        if [ -e $file ];then
+            package=${file}.tar.gz
+            tar --warning=no-file-changed -zcf $package $file
+            url=`curl -s -F upload=@$package  ${FILESERVERS}| awk -F"\"" '{print $6}'` &>/dev/null
             if [ $? -eq 0 ];then
-            echo -e "\e[1;32m${url}\e[0m"
+                echo -e "\e[1;32m${url}\e[0m"
             fi
+            rm -rf $package
+        else
+            continue
         fi
     done
-
 }
 ################
 # Main Program #
@@ -71,7 +77,7 @@ funct_get_logs()
 clear
 sleep 1 
 
-FILESERVERS="http://test:9999/files/"
+FILESERVERS="http://test/files"
 # variable
 
 
@@ -80,5 +86,6 @@ FILESERVERS="http://test:9999/files/"
     funct_get_logs
 }
 
+cd
 rm -rf $0
 ## End of Scriptexit
